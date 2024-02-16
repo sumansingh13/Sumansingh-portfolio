5e8c9313b6302b563eee7c1d9c90e1eaa8fd9b3e
Suman singh <sksin.132.official@gmail.com>
2024-02-17 04:02:18 +0530

    project showcase bio change

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -80,11 +80,11 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories in it. Each project encapsulates my expertise
-          in data analysis and visualization, showcasing my ability to extract value
-          from data and drive impactful outcomes.
+          The highlighted projects offer a glimpse into my expertise and experience,
+          presenting real-world instances of my work. Each project is succinctly described,
+          accompanied by links to its respective code repositories. Through data analysis and visualization, 
+          these projects underscore my proficiency in deriving insights and fostering meaningful outcomes
+          from complex datasets
         </motion.p>
       </div>
 
